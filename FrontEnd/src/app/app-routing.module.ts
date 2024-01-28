9768c633d6c5141a4e87dc3a86c6e8a0a9fce426
Cesar Duque <cesar.duque01@epn.edu.ec>
2024-01-28 00:52:28 -0500

    modulos
    agregue los modulos

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HabitacionesComponent } from './components/habitaciones/habitaciones.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  {path:'inicio',component:HomeComponent},
+  {path:'habitaciones',component:HabitacionesComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
