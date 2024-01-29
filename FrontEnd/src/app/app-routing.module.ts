77ba18ed811afddc9ee15cd61ded289bbc7d8422
Jeniffer <jeniffer_1002@outlook.com>
2024-01-28 20:04:48 -0500

    Hola

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HabitacionesComponent } from './components/habitaciones/habitaciones.component';
-import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-  {path:'inicio',component:HomeComponent},
-  {path:'habitaciones',component:HabitacionesComponent}
-
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { HabitacionesComponent } from './components/habitaciones/habitaciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    HabitacionesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
